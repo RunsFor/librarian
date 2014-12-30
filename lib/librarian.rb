2829f81130044fd8dfe4810876c122979de6a49d
Ilya Konyukhov <runsfor@gmail.com>
2014-12-31 02:29:19 +1000

    Now it can compare files using md5, sha1 or sha2

--- lib/librarian.rb
+++ lib/librarian.rb
@@ -1,3 +1,5 @@
+require 'digest'
+
 class Librarian
   attr_reader :path
 
@@ -9,20 +11,10 @@ class Librarian
     all.select { |book| File.basename(book).match(query) }
   end
 
-  def dublicates
-    grouped_by_filename = all.inject({}) do |collector, book|
-      name = File.basename(book)
-
-      if collector.has_key?(name)
-        collector[name].push(book)
-      else
-        collector[name] = [book]
-      end
-
-      collector
-    end
-
-    grouped_by_filename.select { |_, books| books.size > 1 }
+  # Available types: [ :sha1, :sha2, :md5, :filename ]
+  # @return Hash
+  def dublicates(type: :filename)
+    group_by(type, all).select { |_, books| books.size > 1 }
   end
 
   def all
@@ -30,7 +22,34 @@ class Librarian
     file_pattern = "*.{#{supported_formats.join(',')}}"
     lookup_path = File.join(path, '**', file_pattern)
 
-    Dir.glob(lookup_path)
+    Dir.glob(lookup_path).select{ |path| File.file?(path) }
+  end
+
+  private
+
+  def group_by(type, books)
+    books.inject({}) do |collector, book|
+      case type
+      when :filename
+        key = File.basename(book)
+      when :md5
+        key = Digest::MD5.hexdigest(File.read(book))
+      when :sha1
+        key = Digest::SHA1.hexdigest(File.read(book))
+      when :sha2
+        key = Digest::SHA2.hexdigest(File.read(book))
+      else
+        raise 'Wrong grouping type'
+      end
+
+      if collector.has_key?(key)
+        collector[key].push(book)
+      else
+        collector[key] = [book]
+      end
+
+      collector
+    end
   end
 end
 
--- spec/librarian_spec.rb
+++ spec/librarian_spec.rb
@@ -26,23 +26,87 @@ describe Librarian do
   context '#dublicates' do
     subject { Librarian.new.dublicates }
 
-    let(:different_books) { %w(Maths/numbers_theory.pdf Other/jokes.pdf) }
-    let(:dublicate_books) { %w(Maths/basic_theory.pdf Other/basic_theory.pdf) }
-
     it 'returns Hash' do
       expect(subject).to be_a_kind_of(Hash)
     end
 
-    it 'excludes uniq books from the result' do
-      expect_any_instance_of(Librarian).to receive(:all) { different_books }
+    describe 'comparing types' do
+      context 'when :filename' do
+        subject { Librarian.new.dublicates(type: :filename) }
 
-      expect(subject).to eq({})
-    end
+        let(:different_books) { %w(Maths/numbers_theory.pdf Other/jokes.pdf) }
+        let(:dublicate_books) { %w(Maths/basic_theory.pdf Other/basic_theory.pdf) }
 
-    it 'puts dublicates under one key based on the file name' do
-      expect_any_instance_of(Librarian).to receive(:all) { dublicate_books }
+        it 'excludes uniq books from the result' do
+          expect_any_instance_of(Librarian).to receive(:all) { different_books }
 
-      expect(subject['basic_theory.pdf'].size).to eq(2)
+          expect(subject).to eq({})
+        end
+
+        it 'puts dublicates under one key based on the file name' do
+          expect_any_instance_of(Librarian).to receive(:all) { dublicate_books }
+
+          expect(subject['basic_theory.pdf'].size).to eq(2)
+        end
+      end
+
+      context 'when :sha1' do
+        subject { Librarian.new.dublicates(type: :sha1) }
+
+        let(:numbers_theory) do
+          file = Tempfile.new([ 'numbers_theory', '.pdf' ])
+          file.write('some interesting numbers theory')
+          file.close
+          file
+        end
+
+        let(:jokes) do
+          file = Tempfile.new([ 'jokes', '.pdf' ])
+          file.write('some funny jokes')
+          file.close
+          file
+        end
+
+        let(:mathematics) do
+          file = Tempfile.new([ 'mathematics', '.pdf' ])
+          file.write('some interesting numbers theory')
+          file.close
+          file
+        end
+
+        let(:dublicate_books) { [ numbers_theory.path, mathematics.path ] }
+        let(:different_books) { [ numbers_theory.path, jokes.path ] }
+
+        it 'puts dublicates under one key even if filenames are distinct' do
+          expect_any_instance_of(Librarian).to receive(:all) { dublicate_books }
+
+          dublicate_key = subject.keys.first
+          expect(subject[dublicate_key].size).to eq(2)
+        end
+
+        it 'excludes uniq books from the result even if filenames are the same' do
+          expect_any_instance_of(Librarian).to receive(:all) { different_books }
+
+          expect(subject.size).to eq(0)
+        end
+
+        context 'when directory has filetype at the end' do
+          let(:directory) do
+            Dir.mktmpdir(['math', '.pdf'], '/tmp')
+          end
+
+          let(:all_books) do
+            [ numbers_theory.path, mathematics.path, jokes.path, directory ]
+          end
+
+          it 'skips this directory' do
+            expect(Dir).to receive(:glob) { all_books }
+
+            expect{ subject }.to_not raise_error
+            expect(subject.size).to eq(1)
+          end
+        end
+      end
     end
   end
 end
