5faa6811ed709dbcf584097c99ff798489013b86
Ilya Konyukhov <runsfor@gmail.com>
2014-12-30 16:35:49 +1000

    First simple version of librarian. It can:
    * Search for filenames by the given string
    * Get the listing of all books
    * Check for dublicates based on filenames
    
    Supported filenames are:
    
    * pdf
    * doc
    * mobi
    * epub
    * fb2
    * djvu
    * txt

--- lib/librarian.rb
+++ lib/librarian.rb
@@ -1,3 +1,36 @@
 class Librarian
+  attr_reader :path
+
+  def initialize(path = './')
+    @path = path
+  end
+
+  def search(query = '')
+    all.select { |book| File.basename(book).match(query) }
+  end
+
+  def dublicates
+    grouped_by_filename = all.inject({}) do |collector, book|
+      name = File.basename(book)
+
+      if collector.has_key?(name)
+        collector[name].push(book)
+      else
+        collector[name] = [book]
+      end
+
+      collector
+    end
+
+    grouped_by_filename.select { |_, books| books.size > 1 }
+  end
+
+  def all
+    supported_formats = %i(doc pdf mobi epub djvu txt fb2)
+    file_pattern = "*.{#{supported_formats.join(',')}}"
+    lookup_path = File.join(path, '**', file_pattern)
+
+    Dir.glob(lookup_path)
+  end
 end
 
--- spec/librarian_spec.rb
+++ spec/librarian_spec.rb
@@ -1,5 +1,49 @@
 require 'spec_helper'
 
 describe Librarian do
+  it { should respond_to :all }
+  it { should respond_to :search }
+  it { should respond_to :dublicates }
+
+  context '#all' do
+    it 'returns an Array' do
+      expect(subject.all).to be_a_kind_of(Array)
+    end
+  end
+
+  context '#search' do
+    let(:books) { %w(java/java_tutorial.pdf java/features.pdf) }
+
+    before do
+      expect_any_instance_of(Librarian).to receive(:all) { books }
+    end
+
+    it 'does search based on filename' do
+      expect(subject.search('java')).to match_array(['java/java_tutorial.pdf'])
+    end
+  end
+
+  context '#dublicates' do
+    subject { Librarian.new.dublicates }
+
+    let(:different_books) { %w(Maths/numbers_theory.pdf Other/jokes.pdf) }
+    let(:dublicate_books) { %w(Maths/basic_theory.pdf Other/basic_theory.pdf) }
+
+    it 'returns Hash' do
+      expect(subject).to be_a_kind_of(Hash)
+    end
+
+    it 'excludes uniq books from the result' do
+      expect_any_instance_of(Librarian).to receive(:all) { different_books }
+
+      expect(subject).to eq({})
+    end
+
+    it 'puts dublicates under one key based on the file name' do
+      expect_any_instance_of(Librarian).to receive(:all) { dublicate_books }
+
+      expect(subject['basic_theory.pdf'].size).to eq(2)
+    end
+  end
 end
 
